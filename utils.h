4ce9948a7a85cbc50256508c3f33591f3ece9357
kra53n <greasha46@gmail.com>
2024-10-23 23:07:07 +0300

    add drawc

--- utils.h
+++ utils.h
@@ -4,8 +4,15 @@
 #include "raylib.h"
 
 void drawr(Rectangle r, Color c) {
-    c.a >>= 4;
+    if (c.a >= 200)
+        c.a = 255 / 5;
     DrawRectangleRec(r, c);
+}
+
+void drawc(int x, int y, float r, Color c) {
+    if (c.a >= 200)
+        c.a = 255 / 5;
+    DrawCircle(x, y, r, c);
 }
 
 #endif // UTILS_
